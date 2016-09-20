3f133004a975d80bed61307e024d20a325baa4c6
Robby Ranshous <rranshous@gmail.com>
2016-09-19 21:52:56 -0400

    failing tests

--- spec.rb
+++ spec.rb
@@ -5,11 +5,17 @@ require_relative 'app'
 describe MagicBox do
 
   let(:ear) { double("ear", whisper: nil) }
-  subject { described_class.new(ear) }
+  let(:mouth) { double("mouth", listen: response) }
+  let(:response) { 'i like making friends' }
+  subject { described_class.new(ear, mouth) }
 
   it 'asks you for a secret' do
     expect(ear).to receive(:whisper).with('tell me a secret')
     subject.poke
+  end
+
+  it 'is rude to you' do
+    expect(ear).to receive(:whisper).with("#{response} FART")
   end
 
 end
