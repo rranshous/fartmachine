c537b34d222a2581914cd62ec8a2320386c67f57
Robby Ranshous <rranshous@gmail.com>
2016-09-19 21:50:45 -0400

    pass test

--- app.rb
+++ app.rb
@@ -1,2 +1,10 @@
 
+class MagicBox
+  def initialize ear
+    @ear = ear
+  end
 
+  def poke
+    @ear.whisper 'tell me a secret'
+  end
+end
--- spec.rb
+++ spec.rb
@@ -5,7 +5,7 @@ require_relative 'app'
 describe MagicBox do
 
   let(:ear) { double("ear", whisper: nil) }
-  subject { described_class(ear) }
+  subject { described_class.new(ear) }
 
   it 'asks you for a secret' do
     expect(ear).to receive(:whisper).with('tell me a secret')
